b57b0d61228c58c8c978822836167271d817d86a
Rohit Mittal <mittalrohit7@gmail.com>
2022-06-06 14:19:35 +0530

    fisrdt commit

---
+++ src/main/java/com/example/demo/CloudrestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudrestApplication.class, args);
+	}
+
+}
